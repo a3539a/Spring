810ac8438a22cd42340e077354438f2bbdfedb59
a3539a <a3539a@gmail.com>
2021-07-21 11:18:35 +0900

    2021/07/21 SpringBoot Kmarket 상품 카테고리 완료

--- Kmarket/src/main/java/kr/co/kmarket/dao/admin/AdminProductDao.java
+++ Kmarket/src/main/java/kr/co/kmarket/dao/admin/AdminProductDao.java
@@ -1,8 +1,25 @@
 package kr.co.kmarket.dao.admin;
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
-@Repository
-public class AdminProductDao {
+import kr.co.kmarket.vo.Cate1Vo;
+import kr.co.kmarket.vo.Cate2Vo;
+import kr.co.kmarket.vo.ProductVo;
 
+@Repository
+public interface AdminProductDao {
+	
+	public void insertProduct(ProductVo vo);
+	public ProductVo selectProduct();
+	public List<ProductVo> selectProducts();
+	public void updateProduct(ProductVo vo);
+	public void deleteProduct(ProductVo vo);
+	
+	// 1차 카테고리 가져오기
+	public List<Cate1Vo> selectCate1();
+	
+	// 2차 카테고리 가져오기
+	public List<Cate2Vo> selectCate2(int cate1);
 }
--- Kmarket/src/main/java/kr/co/kmarket/service/admin/AdminProductService.java
+++ Kmarket/src/main/java/kr/co/kmarket/service/admin/AdminProductService.java
@@ -1,8 +1,48 @@
 package kr.co.kmarket.service.admin;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import kr.co.kmarket.dao.admin.AdminProductDao;
+import kr.co.kmarket.vo.Cate1Vo;
+import kr.co.kmarket.vo.Cate2Vo;
+import kr.co.kmarket.vo.ProductVo;
 
 @Service
 public class AdminProductService {
+	
+	@Autowired
+	private AdminProductDao dao;
+	
+	public void insertProduct(ProductVo vo) {
+		
+	}
+	
+	public ProductVo selectProduct() {
+		return null;
+	}
+	
+	public List<ProductVo> selectProducts() {
+		return null;
+	}
+	
+	public void updateProduct(ProductVo vo) {
+		
+	}
+	
+	public void deleteProduct(ProductVo vo) {
+		
+	}
+	
+	// 1차 카테고리 가져오기
+	public List<Cate1Vo> selectCate1() {
+		return dao.selectCate1();
+	}
 
+	// 2차 카테고리 가져오기
+	public List<Cate2Vo> selectCate2(int cate1) {
+		return dao.selectCate2(cate1);
+	}
 }
