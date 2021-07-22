3227793545710febd576543357bcd8b09a731e6f
a3539a <a3539a@gmail.com>
2021-07-22 11:00:34 +0900

    2021/07/22 SpringBoot Kmarket /categories 1차 완료

--- Kmarket/src/main/java/kr/co/kmarket/controller/MainController.java
+++ Kmarket/src/main/java/kr/co/kmarket/controller/MainController.java
@@ -1,13 +1,27 @@
 package kr.co.kmarket.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import kr.co.kmarket.service.MainService;
+import kr.co.kmarket.vo.CategoriesVo;
 
 @Controller
 public class MainController {
 	
+	@Autowired
+	private MainService service;
+	
 	@GetMapping(value = {"", "/index"})
 	public String index() {
 		return "/index";
+	}
+	
+	@GetMapping("/categories")
+	public List<CategoriesVo> getCategories() {
+		return service.selectCategories();
 	}
 }
--- Kmarket/src/main/java/kr/co/kmarket/dao/MainDao.java
+++ Kmarket/src/main/java/kr/co/kmarket/dao/MainDao.java
@@ -1,8 +1,14 @@
 package kr.co.kmarket.dao;
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
-@Repository
-public class MainDao {
+import kr.co.kmarket.vo.CategoriesVo;
 
+@Repository
+public interface MainDao {
+
+	public List<CategoriesVo> selectCategories();
+	
 }
